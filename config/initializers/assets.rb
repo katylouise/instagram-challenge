7703e8cf4d1a2b6c98ffd12b4af08ccf2a6b48fe
Rebecca Appleyard <rklappleyard@gmail.com>
2015-10-12 14:04:44 +0100

    Sign in styling done

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,4 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( comments.css )
 Rails.application.config.assets.precompile += %w( normalize.css )
+Rails.application.config.assets.precompile += %w( user-reg.css )
